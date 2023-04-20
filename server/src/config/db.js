2a5d0ce793f94eb34bf98b9669507fdd0121667a
Harsha <harshavardhan0626@gmail.com>
2023-04-20 14:21:22 +0530

    Node Mongo App

--- server/src/config/db.js
+++ server/src/config/db.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 module.exports = {
-  url: 'mongodb://localhost:27017/Mymongodb', // Replace 'Mymongodb' with the name of your database
+  url: `mongodb://${process.env.DB_HOST}/${process.env.DB_DATABASE}`, // Replace 'Mymongodb' with the name of your database
 };
